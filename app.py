b52cd26cd3d11b2656761209a53bf965a13fa4f3
Thong Tran <tvthong1995@gmail.com>
2018-06-09 22:32:26 +0700

    handle register and login in app.py

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, redirect, url_for, flash, request
+from db import db
 from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from models.user import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '881e75131983b16650ecd36fd815997e';
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db';
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'index'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/register', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    login_form = LoginForm()
+    show_form = 'login' if request.args.get('show_form') == 'login' else 'register'
     registration_form = RegistrationForm()
-    return render_template('index.html', login_form=login_form, registration_form=registration_form)
+    login_form = LoginForm()
+    if registration_form.validate_on_submit():
+        flash('Thanks for registering')
+        user = User.query.filter((User.username==registration_form.username.data) | (User.email==registration_form.email.data)).first()
+        if not user:
+            hashed_password = generate_password_hash(registration_form.password.data)
+            new_user = User(username=registration_form.username.data, email=registration_form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index', show_form='login'))
+        return '<h1>Invalid username or password!</h1>'
+    return render_template('index.html', registration_form=registration_form, login_form=login_form, show_form=show_form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -4,10 +4,10 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=8, max=80), EqualTo('password', message='Password must match!')])
     submit = SubmitField('Sign Up')
 
 
